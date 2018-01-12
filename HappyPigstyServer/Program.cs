5eea7df13656fe6e6c05241b3492a25adb6fa96b
Patipon Taweechat <57050269@kmitl.ac.th>
2018-01-12 23:30:06 +0700

    Add NETPIE to Utilities and Link to Main

--- HappyPigstyServer/Program.cs
+++ HappyPigstyServer/Program.cs
@@ -1,65 +1,23 @@
 ﻿using System;
-using System.Threading;
-using io.netpie.microgear;
+using Utilities;
 
 namespace HappyPigstyServer
 {
     class Program
     {
-        private static String AppID = "HappyPigsty";
-        private static String Key = "zw0HnvDcL9VY4qA";
-        private static String Secret = "BDiOw4Twn98leHH6ATHDkV7fD";
-        private static Microgear microgear;
-
         static void Main(string[] args)
         {
-            initialize();
+            NETPIE netPie = new NETPIE("HappyPigsty", 
+                                       "zw0HnvDcL9VY4qA", 
+                                       "BDiOw4Twn98leHH6ATHDkV7fD");
+            netPie.getMicrogear().onMessage += Receiving;
+            netPie.initialize();
+            netPie.connect("Server", "/topic");
         }
 
-        static void initialize()
+        public static void Receiving(string topic, string message)
         {
-            microgear = new Microgear();
-            microgear.onConnect += Connect;
-            microgear.onMessage += Message;
-            microgear.onAbsent += Absent;
-            microgear.onPresent += Present;
-            microgear.onError += Error;
-            microgear.onInfo += Info;
-            microgear.Connect(AppID, Key, Secret);
-            microgear.SetAlias("Server");
-            microgear.Subscribe("/topic");
+            Console.WriteLine("Receiving message : " + message);
         }
-
-        public static void Connect()
-        {
-            Console.WriteLine("Now I'm connecting with NETPIE");
-        }
-
-        public static void Message(string topic, string message)
-        {
-            Console.WriteLine(topic + " " + message);
-        }
-
-        public static void Present(string token)
-        {
-            Console.WriteLine(token);
-        }
-
-        public static void Absent(string token)
-        {
-            Console.WriteLine(token);
-        }
-
-        public static void Error(string error)
-        {
-            Console.WriteLine(error);
-        }
-
-        public static void Info(string info)
-        {
-            Console.WriteLine(info);
-        }
-
-
     }
 }
---
+++ HappyPigstyServer/Utilities/NetPie.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using io.netpie.microgear;
+using System.Threading;
+
+namespace Utilities
+{
+    class NETPIE
+    {
+        private String AppID;
+        private String Key;
+        private String Secret;
+        private static Microgear microgear;
+
+        public NETPIE(String _AppID, String _Key, String _Secret)
+        {
+            AppID = _AppID;
+            Key = _Key;
+            Secret = _Secret;
+        }
+
+        public void initialize()
+        {
+            microgear = new Microgear();
+            microgear.onConnect += Connect;
+            microgear.onAbsent += Absent;
+            microgear.onPresent += Present;
+            microgear.onError += Error;
+            microgear.onInfo += Info;
+        }
+
+        public Microgear getMicrogear()
+        {
+            return microgear;
+        }
+
+        public void connect(String Alias, String Subscribe)
+        {
+            Console.WriteLine("Connecting to NETPIE....");
+            microgear.Connect(AppID, Key, Secret);
+            microgear.SetAlias(Alias);
+            microgear.Subscribe(Subscribe);
+        }
+
+        public static void Connect()
+        {
+            Console.WriteLine("Connected!");
+        }
+
+        public static void Present(string token)
+        {
+            Console.WriteLine(token);
+        }
+
+        public static void Absent(string token)
+        {
+            Console.WriteLine(token);
+        }
+
+        public static void Error(string error)
+        {
+            Console.WriteLine(error);
+        }
+
+        public static void Info(string info)
+        {
+            Console.WriteLine(info);
+        }
+    }
+}
